80a7f9ed0efbff3c7ecd1fdacb772499886a88f1
salihbektas <sbektas@gtu.edu.tr>
2022-10-10 22:19:57 +0300

    add random color generator

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const digits = [
+    "0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"
+  ];
+
+  function randomColorGenerator(){
+    let color = "#";
+
+    for(let i = 0; i < 6; ++i){
+      color += digits[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+  }
+
+  const [color, setColor] = useState(randomColorGenerator());
+
+  console.log(color);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={{height:100, aspectRatio:1, backgroundColor:color}} />
     </View>
   );
 }
