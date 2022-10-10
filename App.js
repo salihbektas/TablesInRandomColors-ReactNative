aa5094d20b5d746bb207afeba06c75dd35517319
salihbektas <sbektas@gtu.edu.tr>
2022-10-11 00:36:42 +0300

    add tiling of colors

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+
+const {height: HEIGHT, width: WIDTH} = Dimensions.get("screen");
 
 export default function App() {
+  const EDGE = 10;
+
   const digits = [
     "0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"
   ];
+
+
+  let colorArray = Array(Math.ceil(HEIGHT/EDGE));
+
+  for(let i = 0; i < colorArray.length; ++i)
+    colorArray[i] = Array(Math.ceil(WIDTH/EDGE));
+  
 
   function randomColorGenerator(){
     let color = "#";
@@ -17,24 +28,36 @@ export default function App() {
     return color;
   }
 
-  const [color, setColor] = useState(randomColorGenerator());
+  const [colors, setColors] = useState([]);
 
-  console.log(color);
+  useEffect(()=>{
+    let newArr = [...colorArray];
+    for(let i = 0; i < colorArray.length; ++i){
+      for(let j = 0; j < colorArray[i].length; ++j){
+        newArr[i][j] = randomColorGenerator();
+      }
+    }
 
+    setColors(newArr);
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={{height:100, aspectRatio:1, backgroundColor:color}} />
+    <View style={styles.page}>
+      {colors.map((row, index1) => 
+        <View style={styles.row} key={index1}>
+          {row.map((item, index) => <View style={{height: EDGE, aspectRatio: 1, backgroundColor: item}} key={index} />)}
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  row: {
+    flexDirection: "row",
+  }
 });
